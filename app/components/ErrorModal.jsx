5e417af7f2d1d098ef582178f3939eed364e60d1
Andres Caycho <acaycho@belatrix.com>
2017-01-25 13:32:16 -0500

    Fixing issues with ErrorModal.jsx

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function(){
@@ -11,13 +13,8 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
   componentDidMount: function(){
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function() {
     var {title, message} = this.props;
-
-    return (
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
           <h4>{title}</h4>
           <p>{message}</p>
@@ -26,6 +23,23 @@ var ErrorModal = React.createClass({
                 Okay
             </button>
           </p>
+      </div>
+    );
+
+    //Transfor jsx to jquery elemnt
+    //ReactDOMServer.renderToString to pass jsx to string
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function() {
+    //Error appears when if we handle modal on render, Foundation manipulates
+    //DOM and React drops error. To solve this issue we pass all component
+    //inside 'componentDidMount' and insert JSXModal using jquery
+    return(
+      <div>
       </div>
     );
   }
